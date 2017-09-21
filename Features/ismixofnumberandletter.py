cb9ae5f10170cca54c6df149a6e4ce936ab50978
timsyang <31863237+timsyang@users.noreply.github.com>
2017-09-20 22:20:48 -0400

    Update ismixofnumberandletter.py

--- Features/ismixofnumberandletter.py
+++ Features/ismixofnumberandletter.py
@@ -14,19 +14,19 @@ def ismixofnumberandletter(a):
     next_element=np.append(a[1:,3],"abc")
     this_element=a[:,3]
     previous_element=np.insert(a[:-1,3],0,"abc")
-    numbers=np.array(['0123456789'])
-    letters=np.array(['QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'])
+    numbers='0123456789'
+    letters='QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
     for j in range(1,np.shape(a)[1]):
         countnumbers=0
         countletters=0
         for k in range(1,len(this_element[j-1])):
-            if this_element[j-1][k-1] in numbers[0]:
+            if this_element[j-1][k-1] in numbers:
                 countnumbers=countnumbers+1
-            if this_element[j-1][k-1] in letters[0]:
+            if this_element[j-1][k-1] in letters:
                 countletters=countletters+1
         output[j,1]=a[j,1]
         output[j,2]=a[j,2]
-        if (countnumbers>0)&(countletters>0):
+        if countnumbers+countletters==len(this_element[j-1]):
             output[j,3]=1
         else:
             output[j,3]=0
