556c51de61b8c3f5c1f2f49268dcbad8c09310b4
timsyang <31863237+timsyang@users.noreply.github.com>
2017-09-22 17:22:33 -0400

    Add files via upload

---
+++ Features/ismixoflettersandspecialcharacters.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 21:47:53 2017
+
+@author: timschutzlkord
+"""
+
+import numpy as np
+ 
+def ismixoflettersandspecialcharacters(a):
+    # accept input argument as numpy array of n rows and 3 columns
+    output=np.array(shape=(np.shape(a)[1],3))
+    next_element=np.append(a[1:,3],"abc")
+    this_element=a[:,3]
+    previous_element=np.insert(a[:-1,3],0,"abc")
+    letters='QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
+    punctuationsnumbersandletters=',.-_()+!? :;[]\'\"/0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
+    for j in range(1,np.shape(a)[1]):
+        countletters=0
+        countspecialcharacters=0
+        for k in range(1,len(this_element[j-1])):
+            if this_element[j-1][k-1] in letters:
+                countletters=countletters+1
+            if not(this_element[j-1][k-1] in punctuationsnumbersandletters):
+                countspecialcharacters=countspecialcharacters+1
+        output[j-1,1]=a[j-1,1]
+        output[j-1,2]=a[j-1,2]
+        if countletters+countspecialcharacters==len(this_element[j-1]):
+            output[j-1,3]=1
+        else:
+            output[j-1,3]=0
+    return output
