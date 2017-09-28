a85de7d284a04ac518d3db2965db8f1c9fd03ddf
timsyang <31863237+timsyang@users.noreply.github.com>
2017-09-28 14:37:49 -0400

    This works after testing

--- Features/ifallcaps.py
+++ Features/ifallcaps.py
@@ -1,23 +1,37 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from collections import Counter
+"""
+Created on Wed Sep 20 14:10:03 2017
+
+@author: timschutzlkord
+"""
+
 import numpy as np
-def step(x):
-    return 1 * (x > 0)
+import pandas as pd
+test = pd.read_csv('en_train_lim.csv')
+ 
 def ifallcaps(a):
     # accept input argument as numpy array of n rows and 3 columns
-    output=np.zeros(shape=(np.shape[1],3))
-    next_element=np.append(a[1:,3],"abc")
-    this_element=a[:,3]
-    previous_element=np.insert(a[:-1,3],0,"abc")
-    next_element=next_element.astype('|S1')
-    this_element=this_element.astype('|S1')
-    previous_element=previous_element.astype('|S1')
-    letters=np.array(['QWERTYUIOPASDFGHJKLZXCVBNM'])
-    for j in range(np.shape[1]):
-        cnt1=Counter(previous_element[j])
-        cnt2=Counter(this_element[j])
-        cnt3=Counter(next_element[j])
-        output[j,0]=step(sum(cnt1[x] for x in letters))
-        output[j,1]=step(sum(cnt2[x] for x in letters))
-        output[j,2]=step(sum(cnt3[x] for x in letters))
+    output=np.zeros(shape=(np.shape(a)[0],3))
+    next_element=np.append(a[1:,2],"abc")
+    this_element=a[:,2]
+    previous_element=np.insert(a[:-1,2],0,"abc")
+    caps='QWERTYUIOPASDFGHJKLZXCVBNM'
+    for j in range(1,np.shape(a)[0]+1):
+        count=0
+        for k in range(1,len(str(this_element[j-1]))+1):
+            if str(this_element[j-1])[k-1] in caps:
+                count=count+1            
+        output[j-1,0]=a[j-1,0]
+        output[j-1,1]=a[j-1,1]
+        if count==len(str(this_element[j-1])):
+            output[j-1,2]=1
+        else:
+            output[j-1,2]=0
     return output
+
+test=test.values
+output=ifallcaps(test)
+output
+output1 = pd.DataFrame(output)
+output1.to_csv('output1.csv')
