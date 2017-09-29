e4089bfd1702a9640e58ca724152e5e9c87a954a
timsyang <31863237+timsyang@users.noreply.github.com>
2017-09-28 21:20:58 -0400

    This works after testing

--- Features/numberofcommas.py
+++ Features/numberofcommas.py
@@ -3,25 +3,32 @@
 """
 Created on Wed Sep 20 15:50:16 2017
 
-@author: timschutzlkord, based on Michael's script
+@author: timschutzlkord
 """
 
 import numpy as np
+import pandas as pd
+test = pd.read_csv('en_train_lim.csv')
  
 def numberofcommas(a):
     # accept input argument as numpy array of n rows and 3 columns
-    output=np.array(shape=(np.shape(a)[1],3))
-    next_element=np.append(a[1:,3],"abc")
-    this_element=a[:,3]
-    previous_element=np.insert(a[:-1,3],0,"abc")
+    output=np.zeros(shape=(np.shape(a)[0],3))
+    next_element=np.append(a[1:,2],"abc")
+    this_element=a[:,2]
+    previous_element=np.insert(a[:-1,2],0,"abc")
     comma=','
-    for j in range(1,np.shape(a)[1]):
+    for j in range(1,np.shape(a)[0]+1):
         count = 0
-        for k in range(1,len(this_element[j-1])):
-            if this_element[j-1][k-1] in comma:
+        for k in range(1,len(str(this_element[j-1]))+1):
+            if str(this_element[j-1])[k-1] in comma:
                 count = count +1
+        output[j-1,0]=a[j-1,0]
         output[j-1,1]=a[j-1,1]
-        output[j-1,2]=a[j-1,2]
-        output[j-1,3]=count
+        output[j-1,2]=count
     return output
     
+test=test.values
+output=numberofcommas(test)
+output
+output1 = pd.DataFrame(output)
+output1.to_csv('output1.csv')
