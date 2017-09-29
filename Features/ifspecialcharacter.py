4a40af61d0a798152e56904f4c881c04f9f2680d
timsyang <31863237+timsyang@users.noreply.github.com>
2017-09-28 21:20:26 -0400

    This works after testing

--- Features/ifspecialcharacter.py
+++ Features/ifspecialcharacter.py
@@ -3,22 +3,32 @@
 """
 Created on Thu Sep 20 13:36:53 2017
 
-@author: timschutzlkord, based on Michael's script
+@author: timschutzlkord, checked
 """
 
 import numpy as np
+import pandas as pd
+test = pd.read_csv('en_train_lim.csv')
  
 def ifspecialcharacter(a):
     # accept input argument as numpy array of n rows and 3 columns
-    output=np.array(shape=(np.shape(a)[1],3))
-    next_element=np.append(a[1:,3],"abc")
-    this_element=a[:,3]
-    previous_element=np.insert(a[:-1,3],0,"abc")
-    notspecialcharacter='QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789'
-    for j in range(1,np.shape(a)[1]):
-        isstrenglength1_this_element[j]=int(len(this_element[j])==1)
-        isspecialcharacter_this_element[j]=int(not(this_element[j] in notspecialcharacter))
-        output[j,1]=a[j,1]
-        output[j,2]=a[j,2]
-        output[j,3]=isstrenglength1_this_element[j]*isspecialcharacter[j]
+    output=np.zeros(shape=(np.shape(a)[0],3))
+    next_element=np.append(a[1:,2],"abc")
+    this_element=a[:,2]
+    previous_element=np.insert(a[:-1,2],0,"abc")
+    notspecialcharacter='QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789,.-_()+!? :;[]\'\"/'
+    isstrlen_this_element=np.zeros(shape=(np.shape(a)[0],1))
+    isspecialcharacter_this_element=np.zeros(shape=(np.shape(a)[0],1))
+    for j in range(1,np.shape(a)[0]+1):
+        isstrlen_this_element[j-1]=int(len(str(this_element[j-1]))==1)
+        isspecialcharacter_this_element[j-1]=int((str(this_element[j-1]) not in notspecialcharacter))
+        output[j-1,0]=a[j-1,0]
+        output[j-1,1]=a[j-1,1]
+        output[j-1,2]=isstrlen_this_element[j-1]*isspecialcharacter_this_element[j-1]
     return output
+
+test=test.values
+output=ifspecialcharacter(test)
+output
+output1 = pd.DataFrame(output)
+output1.to_csv('output1.csv')
