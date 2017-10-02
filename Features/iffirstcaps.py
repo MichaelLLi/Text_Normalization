b30a814666fbe668b0f127018a5c86ef2ac353ef
shaznl <31679367+shaznl@users.noreply.github.com>
2017-10-01 21:23:02 -0400

    This works with testing

--- Features/iffirstcaps.py
+++ Features/iffirstcaps.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
-from collections import Counter
 import numpy as np
+import pandas as pd
 def step(x):
     return 1 * (x > 0)
 def iffirstcaps(a):
     # accept input argument as numpy array of n rows and 3 columns
-    output=np.zeros(shape=(np.shape(a)[1],3))
-    next_element=np.append(a[1:,3],"abc")
-    this_element=a[:,3]
-    previous_element=np.insert(a[:-1,3],0,"abc")
-    next_element=next_element.astype('|S1')
-    this_element=this_element.astype('|S1')
-    previous_element=previous_element.astype('|S1')
-    letters=np.array(['QWERTYUIOPASDFGHJKLZXCVBNM'])
-    for j in range(1,np.shape(a)[1]):
-        cnt1=Counter(previous_element[j])
-        cnt2=Counter(this_element[j])
-        cnt3=Counter(next_element[j])
-        output[j,0]=step(sum(cnt1[x] for x in letters))
-        output[j,1]=step(sum(cnt2[x] for x in letters))
-        output[j,2]=step(sum(cnt3[x] for x in letters))
+    output=np.zeros(shape=(np.shape(a)[0],3))
+    next_element=np.append(a[1:,2],"abc")
+    this_element=a[:,2]
+    previous_element=np.insert(a[:-1,2],0,"abc")
+    for j in range(np.shape(a)[0]):
+        nxt=str(next_element[j])[0]
+        ths=str(this_element[j])[0]
+        prev=str(previous_element[j])[0]
+        output[j,0]=int(nxt.isupper())
+        output[j,1]=int(ths.isupper())
+        output[j,2]=int(prev.isupper())
     return output
--- Features/ifpreviousnext.py
+++ Features/ifpreviousnext.py
@@ -7,16 +7,16 @@ Created on Mon Sep 18 16:52:59 2017
 import numpy as np
 def ifpreviousnext(a):
     # accept input argument as numpy array of n rows and 3 columns
-    output=np.zeros(shape=(np.shape(a)[1],2))
+    output=np.zeros(shape=(np.shape(a)[0],2))
     sentence_ID=a[:,0]
-    for j in range(np.shape(a)[1]):
+    for j in range(np.shape(a)[0]):
         if j==0:
             output[j,0]=0
             if sentence_ID[j+1]==sentence_ID[j]:
                 output[j,1]=1
             else:
                 output[j,1]=0
-        elif j==np.shape[1]-1:
+        elif j==np.shape(a)[0]-1:
             output[j,1]=0
             if sentence_ID[j-1]==sentence_ID[j]:
                 output[j,0]=1
