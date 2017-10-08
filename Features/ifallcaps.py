79659c957f3389df3a07c6a4dbf0e1dabf67b769
timsyang <31863237+timsyang@users.noreply.github.com>
2017-10-08 14:31:10 -0400

    Updated Codes

--- Features/ifallcaps.py
+++ Features/ifallcaps.py
@@ -6,9 +6,8 @@ Created on Wed Sep 20 14:10:03 2017
 @author: timschutzlkord
 """
 
+from collections import Counter
 import numpy as np
-import pandas as pd
-test = pd.read_csv('en_train_lim.csv')
  
 def ifallcaps(a):
     # accept input argument as numpy array of n rows and 3 columns
@@ -17,21 +16,14 @@ def ifallcaps(a):
     this_element=a[:,2]
     previous_element=np.insert(a[:-1,2],0,"abc")
     caps='QWERTYUIOPASDFGHJKLZXCVBNM'
-    for j in range(1,np.shape(a)[0]+1):
-        count=0
-        for k in range(1,len(str(this_element[j-1]))+1):
-            if str(this_element[j-1])[k-1] in caps:
-                count=count+1            
-        output[j-1,0]=a[j-1,0]
-        output[j-1,1]=a[j-1,1]
-        if count==len(str(this_element[j-1])):
-            output[j-1,2]=1
-        else:
-            output[j-1,2]=0
+    for j in range(np.shape(a)[0]):
+        cnt_next=Counter(str(next_element[j]))
+        cnt_this=Counter(str(this_element[j]))
+        cnt_prev=Counter(str(previous_element[j]))
+        # for k in range(len(str(this_element[j]))):
+           # if str(this_element[j])[k] in caps:
+              # count=count+1            
+        output[j,0]=int(sum(cnt_next[x] for x in caps)==len(cnt_next))
+        output[j,1]=int(sum(cnt_this[x] for x in caps)==len(cnt_this))
+        output[j,2]=int(sum(cnt_prev[x] for x in caps)==len(cnt_prev))
     return output
-
-test=test.values
-output=ifallcaps(test)
-output
-output1 = pd.DataFrame(output)
-output1.to_csv('output1.csv')
