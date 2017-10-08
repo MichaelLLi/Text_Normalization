3733a0945ac548db78a060c7d4d18fac954a9c06
timsyang <31863237+timsyang@users.noreply.github.com>
2017-10-08 15:29:46 -0400

    Update ifcommafollowedbyanumber.py

--- Features/ifcommafollowedbyanumber.py
+++ Features/ifcommafollowedbyanumber.py
@@ -14,12 +14,17 @@ def ifcommafollowedbynumber(a):
     previous_element=np.insert(a[:-1,2],0,"abc")
     comma=','
     numbers='0123456789'
-    for j in range(1,np.shape(a)[0]+1):
-        identifier = 0
-        for k in range(1,len(str(this_element[j-1]))):
-            if (str(this_element[j-1])[k-1] in comma)&(str(this_element[j-1])[k] in numbers):
-                identifier = 1
-        output[j-1,0]=a[j-1,0]
-        output[j-1,1]=a[j-1,1]
-        output[j-1,2]=identifier
+    for j in range(np.shape(a)[0]):
+        output[j,0]=0
+        output[j,1]=0
+        output[j,2]=0
+        for k in (range(1,len(str(next_element[j]))-1)):
+            if (str(next_element[j])[k-1] in numbers)&(str(next_element[j])[k] in comma)&(str(next_element[j])[k+1] in numbers):
+                output[j,0]=1
+        for l in (range(1,len(str(this_element[j]))-1)):
+            if (str(this_element[j])[l-1] in numbers)&(str(this_element[j])[l] in comma)&(str(this_element[j])[l+1] in numbers):
+                output[j,1]=1
+        for m in (range(1,len(str(previous_element[j]))-1)):
+            if (str(previous_element[j])[m-1] in numbers)&(str(previous_element[j])[m] in comma)&(str(previous_element[j])[m+1] in numbers):
+                output[j,2]=1
     return output
