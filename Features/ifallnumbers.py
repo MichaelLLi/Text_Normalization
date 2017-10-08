b786e4782074eca13ef3713ffb922e192ace1d97
timsyang <31863237+timsyang@users.noreply.github.com>
2017-10-08 14:43:42 -0400

    Update ifallnumbers.py

--- Features/ifallnumbers.py
+++ Features/ifallnumbers.py
@@ -6,32 +6,27 @@ Created on Wed Sep 20 19:57:05 2017
 @author: timschutzlkord
 """
 
+from collections import Counter
 import numpy as np
-import pandas as pd
-test = pd.read_csv('en_train_lim.csv')
  
-def isallnumbers(a):
+def ifallnumbers(a):
     # accept input argument as numpy array of n rows and 3 columns
     output=np.zeros(shape=(np.shape(a)[0],3))
     next_element=np.append(a[1:,2],"abc")
     this_element=a[:,2]
     previous_element=np.insert(a[:-1,2],0,"abc")
-    numbers='0123456789'
-    for j in range(1,np.shape(a)[0]+1):
-        countnumbers=0
-        for k in range(1,len(str(this_element[j-1]))+1):
-            if str(this_element[j-1])[k-1] in numbers:
-                countnumbers=countnumbers+1
-        output[j-1,0]=a[j-1,0]
-        output[j-1,1]=a[j-1,1]
-        if countnumbers==len(str(this_element[j-1])):
-            output[j-1,2]=1
-        else:
-            output[j-1,2]=0
+    numbers='0123456789,.'
+    for j in range(np.shape(a)[0]):
+        cnt_next=Counter(str(next_element[j]))
+        cnt_this=Counter(str(this_element[j]))
+        cnt_prev=Counter(str(previous_element[j]))          
+        output[j,0]=int(sum(cnt_next[x] for x in numbers)==len(str(next_element[j])))
+        output[j,1]=int(sum(cnt_this[x] for x in numbers)==len(str(this_element[j])))
+        output[j,2]=int(sum(cnt_prev[x] for x in numbers)==len(str(previous_element[j])))
     return output
-    
+
 test=test.values
-output=isallnumbers(test)
+output=ifallnumbers(test)
 output
 output1 = pd.DataFrame(output)
-output1.to_csv('output1.csv')
+output1.to_csv('output1_2.csv')
