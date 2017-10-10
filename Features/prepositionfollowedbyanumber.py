d9ee1b767ba167497f4acaf2a6bf9f98bb67d216
timsyang <31863237+timsyang@users.noreply.github.com>
2017-10-10 12:57:45 -0400

    Update prepositionfollowedbyanumber.py

--- Features/prepositionfollowedbyanumber.py
+++ Features/prepositionfollowedbyanumber.py
@@ -7,9 +7,7 @@ Created on Wed Sep 20 21:57:35 2017
 """
 
 import numpy as np
-import pandas as pd
-test = pd.read_csv('en_train_lim.csv') 
- 
+
 def prepositionfollowedbyanumber(a):
     # accept input argument as numpy array of n rows and 3 columns
     output=np.zeros(shape=(np.shape(a)[0],3))
@@ -18,22 +16,19 @@ def prepositionfollowedbyanumber(a):
     previous_element=np.insert(a[:-1,2],0,"abc")
     numbers='0123456789'
     prepositions=np.array(['on','in','at','since','for','to','before','ago','past','until','till','by','after','from','around','about','of','during'])
-    for j in range(1,np.shape(a)[0]+1):
+    for j in range(np.shape(a)[0]):
         indicatornumber=0
         indicatorpreposition=0
-        if str(this_element[j-1]) in prepositions:
+        if str(this_element[j]) in prepositions:
             indicatorpreposition=1
-        if str(next_element[j-1])[0] in numbers:
+        if str(next_element[j])[0] in numbers:
             indicatornumber=1
-        output[j-1,0]=a[j-1,0]
-        output[j-1,1]=a[j-1,1]
         if indicatorpreposition*indicatornumber==1:
-            output[j-1,2]=1
+            output[j,1]=1
         else:
-            output[j-1,2]=0
+            output[j,1]=0
+    for l in range(2,np.shape(a)[0]+1):
+        output[l-1,2]=output[l-2,1]
+    for m in range(1,np.shape(a)[0]):
+        output[m-1,0]=output[m,1]
     return output
-    
-test=test.values
-output=prepositionfollowedbyanumber(test)
-output
-output1 = pd.DataFrame(output)
