d661fbcd844c038bd06addd0e72226e499b2ffa3
timsyang <31863237+timsyang@users.noreply.github.com>
2018-01-14 14:48:38 -0500

    Update address.py

--- Prediction/address.py
+++ Prediction/address.py
@@ -7,54 +7,84 @@ import re
 letters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
 numbers = '0123456789'
 
-def address(a):
-    output=[None] * len(a)
-    inputaddress=a
-    for j in range(len(a)):
-        inputaddress[j] = re.sub('[- \.,]','',str(inputaddress[j]))
-        cnt = Counter(str(inputaddress[j]))
-        sum_l = sum(cnt[x] for x in letters)
-        sum_n = sum(cnt[x] for x in numbers)
-        l, n = inputaddress[j][:sum_l], inputaddress[j][sum_l:]
-        l = l.lower()
-        if sum_l >=3 :
-            l = ''.join(l)
+def dateyear(a):
+    if isinteger(str(a)):
+        a = int(a)
+        if ((a/100).is_integer()) & ((a/1000).is_integer()) & (a >= 100):
+            o = num2words(int(str(a)[:-3])) + ' ' + 'thousand'
+            o = re.sub(' and','',o)
+            return o
+        elif ((a/100).is_integer()) & ((a/1000).is_integer() == False) & (a >= 100): 
+            o = num2words(int(str(a)[:-2])) + ' ' + 'hundred'
+            o = re.sub(' and','',o)
+            return o
         else:
-            l = ' '.join(l)
-        if (l == 'u s') & (sum_n > 3) :
-            if sum_n == 4:
-                n = num2words(int(str(n[:2]))) + ' ' +  num2words(int(str(n[2:])))
+            if (int(a) >= 2000) & (int(a) <= 2009):
+                o = num2words(int(str(a)))
+                o = re.sub(' and','',o)
+                return o
+            elif len(str(a)) == 4:
+                if str(a)[2] == '0': 
+                    o = num2words(int(str(a)[:2])) + ' ' + 'o' + ' ' + num2words(int(str(a)[3:]))
+                    o = re.sub(' and','',o)
+                    o = re.sub('-',' ',o)
+                    return o
+                else: 
+                    o = num2words(int(str(a)[:2])) + ' ' + num2words(int(str(a)[2:]))
+                    o = re.sub(' and','',o)
+                    o = re.sub('-',' ',o)
+                    return o
+            elif len(str(a)) == 3:
+                if str(a)[1] == '0': 
+                    o = num2words(int(str(a)[:1])) + ' ' + 'o' + ' ' + num2words(int(str(a)[2:]))
+                    o = re.sub(' and','',o)
+                    o = re.sub('-',' ',o)
+                    return o
+                else: 
+                    o = num2words(int(str(a)[:1])) + ' ' + num2words(int(str(a)[1:]))
+                    o = re.sub(' and','',o)
+                    o = re.sub('-',' ',o)
+                    return o
+            elif (len(str(a)) == 2) | (len(str(a)) == 1):
+                o = num2words(int(str(a)))
+                o = re.sub(' and','',o)
+                o = re.sub('-',' ',o)
+                return o
+    else:       
+        return a
+    
+def address(a):
+    output=[0] * len(a)
+    inputaddress=a
+    space = ' '
+    for j in range(len(a)):
+        try:
+            inputaddress[j] = re.sub('[.,]','', inputaddress[j])
+            inputaddress[j] = re.sub('[-]',' ', inputaddress[j])
+            cnt = Counter(inputaddress[j])
+            cnt_space = sum(cnt[x] for x in space)
+            orientation = {'e':'east','w': 'west', 'n': 'north', 's' : 'south', 'ne' : 'northeast', 'se' : 'southeast', 'nw' : 'northwest', 'sw' : 'southwest', 'us':'us'}      
+            seg = re.findall(r"[a-zA-Z]+|[0-9]+",str(inputaddress[j]))
+            # if cnt_space == 0:
+            seg[0] = str(seg[0]).lower()        
+            if len(seg[0]) >= 4:
+                seg[0] = ''.join(seg[0])
             else:
-                n = num2words(int(str(n)))
-        else: 
-            if sum_n == 1:
-                n = num2words(int(str(n)))
-            elif sum_n == 2:
-                cnt_n = Counter(str(n))
-                sum_nn = sum(cnt_n[x] for x in letters)
-                if sum_nn == 0:
-                    if str(n[0]) == '0':
-                        n = 'o' + ' ' + num2words(int(str(n[1])))
-                    else:
-                        n = num2words(int(str(n)))
-            elif sum_n == 3:
-                cnt_nn = Counter(str(n))
-                sum_nnn = sum(cnt_nn[x] for x in letters)
-                if sum_nn == 0:
-                    if (str(n[0]) == '0')&(str(n[1]) == '0'):
-                        n = 'o' + ' ' + 'o' + ' ' + num2words(int(str(n)[2]))
-                    elif (str(n[0]) == '0')&(str(n[1]) != '0'):
-                        n = num2words(int(str(n)[1:]))
-                    else:
-                        if (str(n[1]) == '0'):
-                            n = num2words(int(str(n)[0])) + ' '+'o' + ' ' + num2words(int(str(n)[2]))
-                        else: 
-                            n1, n2 = n[0], n[1:]
-                            n1 = num2words(int(str(n1)))
-                            n2 = num2words(int(str(n2)))
-                            n = n1 + ' ' + n2
-            else:
-                n = ' '.join(n)
+                seg[0] = ' '.join(seg[0])
+            if len(seg) == 3:
+                seg[2] = str(seg[2]).lower()  
+                if str(seg[2]) in orientation:
+                    if len(str(seg[1])) >= 3:
+                        output[j] = str(seg[0]) + ' ' + dateyear(int(str(seg[1]))) + ' ' + orientation[str(seg[2]).lower()]
+                        output[j] = re.sub('-',' ',output[j])
+                    else: 
+                        output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1]))) + ' ' + orientation[str(seg[2]).lower()]
+                        output[j] = re.sub('-',' ',output[j])
+                else:
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1]))) + ' ' + str(seg[2])
+                    output[j] = re.sub('-',' ',output[j])
+            elif (cnt_space == 0) & (len(seg[1]) == 1):
+                n = ' '.join(seg[1])
                 n = re.sub('0','o',n)
                 n = re.sub('1','one',n)
                 n = re.sub('2','two',n)
@@ -65,9 +95,184 @@ def address(a):
                 n = re.sub('7','seven',n)
                 n = re.sub('8','eight',n)
                 n = re.sub('9','nine',n)
-        output[j] = l + ' ' + n
-        output[j] = re.sub('-',' ',output[j])
-        output[j] = re.sub('zero','o',output[j])
-        output[j] = re.sub(' and','',output[j])
-        output[j] = re.sub(',','',output[j])
+                output[j] = str(seg[0]) + ' ' + n
+                
+            elif (cnt_space == 0) & (len(seg[1]) == 2):
+                if seg[1][0] != '0':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])))
+                    output[j] = re.sub('zero','o',output[j]) 
+                    output[j] = re.sub('-',' ',output[j])
+                else: 
+                    output[j] = str(seg[0]) + ' ' + 'o' + ' ' + num2words(int(str(seg[1])[-1]))
+                    output[j] = re.sub('zero','o',output[j]) 
+                    output[j] = re.sub('-',' ',output[j])
+            elif (cnt_space == 0) & (len(seg[1]) == 3):
+                if str(seg[1])[-2:] == '00':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[0]))+ ' ' + 'hundred'
+                elif str(seg[1])[:2] == '00':
+                    output[j] = str(seg[0]) + ' o o ' + num2words(int(str(seg[1])[1:]))
+                    output[j] = re.sub('-',' ',output[j])
+                elif str(seg[1])[0] == '0':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[1:]))
+                    output[j] = re.sub('-',' ',output[j])
+                elif (seg[1][-1] != '0'): 
+                    n = ' '.join(seg[1])
+                    n = re.sub('0','o',n)
+                    n = re.sub('1','one',n)
+                    n = re.sub('2','two',n)
+                    n = re.sub('3','three',n)
+                    n = re.sub('4','four',n)
+                    n = re.sub('5','five',n)
+                    n = re.sub('6','six',n)
+                    n = re.sub('7','seven',n)
+                    n = re.sub('8','eight',n)
+                    n = re.sub('9','nine',n)
+                    output[j] = str(seg[0]) + ' ' + n
+                else:
+                    output[j] = str(seg[0]) + ' ' + dateyear(seg[1])
+                    output[j] = re.sub('-',' ',output[j]) 
+            elif (cnt_space == 0) & (len(seg[1]) == 4):
+                if str(seg[1])[-3:] == '000':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[0]))+ ' ' + 'thousand'
+                elif str(seg[1])[-2:] == '00':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[:2]))+ ' ' + 'hundred'
+                    output[j] = re.sub('-',' ',output[j])
+                elif str(seg[1])[:3] == '000':
+                    output[j] = str(seg[0]) + ' o o o ' + num2words(int(str(seg[1])[-1]))
+                elif str(seg[1])[:2] == '00':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[-2:]))
+                    output[j] = re.sub('-',' ',output[j])
+                else:
+                    n = ' '.join(seg[1])
+                    n = re.sub('0','o',n)
+                    n = re.sub('1','one',n)
+                    n = re.sub('2','two',n)
+                    n = re.sub('3','three',n)
+                    n = re.sub('4','four',n)
+                    n = re.sub('5','five',n)
+                    n = re.sub('6','six',n)
+                    n = re.sub('7','seven',n)
+                    n = re.sub('8','eight',n)
+                    n = re.sub('9','nine',n)
+                    output[j] = str(seg[0]) + ' ' + n
+            elif (cnt_space == 0) & (len(seg[1]) >= 5):
+                zeroseg = re.findall(r"[0]+|[0-9]+",seg[1])
+                if len(zeroseg) == 2:
+                    if len(zeroseg[1]) == 2:
+                        output[j] = seg[0] + ' ' + num2words(int(str(zeroseg[1])))
+                        output[j] = re.sub('-',' ',output[j])
+                    elif len(zeroseg[1]) == 1:
+                        n = ' '.join(seg[1])
+                        n = re.sub('0','o',n)
+                        n = re.sub('1','one',n)
+                        n = re.sub('2','two',n)
+                        n = re.sub('3','three',n)
+                        n = re.sub('4','four',n)
+                        n = re.sub('5','five',n)
+                        n = re.sub('6','six',n)
+                        n = re.sub('7','seven',n)
+                        n = re.sub('8','eight',n)
+                        n = re.sub('9','nine',n)
+                        output[j] = str(seg[0]) + ' ' + n
+                    else:
+                        n = ' '.join(seg[1])
+                        n = re.sub('0','o',n)
+                        n = re.sub('1','one',n)
+                        n = re.sub('2','two',n)
+                        n = re.sub('3','three',n)
+                        n = re.sub('4','four',n)
+                        n = re.sub('5','five',n)
+                        n = re.sub('6','six',n)
+                        n = re.sub('7','seven',n)
+                        n = re.sub('8','eight',n)
+                        n = re.sub('9','nine',n)
+                        output[j] = str(seg[0]) + ' ' + n
+                else:
+                    n = ' '.join(seg[1])
+                    n = re.sub('0','o',n)
+                    n = re.sub('1','one',n)
+                    n = re.sub('2','two',n)
+                    n = re.sub('3','three',n)
+                    n = re.sub('4','four',n)
+                    n = re.sub('5','five',n)
+                    n = re.sub('6','six',n)
+                    n = re.sub('7','seven',n)
+                    n = re.sub('8','eight',n)
+                    n = re.sub('9','nine',n)
+                    output[j] = str(seg[0]) + ' ' + n
+            elif (cnt_space >= 1) & (len(seg[1]) <= 2): 
+                output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])))
+                output[j] = re.sub('zero','o',output[j]) 
+                output[j] = re.sub('-',' ',output[j])
+            elif (cnt_space >= 1) & (len(seg[1]) == 3): 
+                if str(seg[1])[-2:] == '00':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[0]))+ ' ' + 'hundred'
+                elif str(seg[1])[:2] == '00':
+                    output[j] = str(seg[0]) + ' o o ' + num2words(int(str(seg[1])[1:]))
+                    output[j] = re.sub('-',' ',output[j])
+                elif str(seg[1])[0] == '0':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[1:]))
+                    output[j] = re.sub('-',' ',output[j])
+                elif (str(seg[1])[0] != '0') & (str(seg[1])[1] != '0'):
+                    output[j] = str(seg[0]) + ' ' + dateyear(str(seg[1]))
+                    output[j] = re.sub('-',' ',output[j]) 
+                elif (str(seg[1])[0] != '0') & (str(seg[1])[1] == '0'):
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1][0]))) + ' ' + 'o' + ' ' + num2words(int(str(seg[1][-1])))
+                else:
+                    n = ' '.join(seg[1])
+                    n = re.sub('0','o',n)
+                    n = re.sub('1','one',n)
+                    n = re.sub('2','two',n)
+                    n = re.sub('3','three',n)
+                    n = re.sub('4','four',n)
+                    n = re.sub('5','five',n)
+                    n = re.sub('6','six',n)
+                    n = re.sub('7','seven',n)
+                    n = re.sub('8','eight',n)
+                    n = re.sub('9','nine',n)
+                    output[j] = str(seg[0]) + ' ' + n
+            elif (cnt_space >= 1) & (len(seg[1]) == 4): 
+                if str(seg[1])[-3:] == '000':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[0]))+ ' ' + 'thousand'
+                elif str(seg[1])[-2:] == '00':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[:2]))+ ' ' + 'hundred'
+                    output[j] = re.sub('-',' ',output[j])
+                elif str(seg[1])[:3] == '000':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[-1]))
+                elif str(seg[1])[:2] == '00':
+                    output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])[-2:]))
+                    output[j] = re.sub('-',' ',output[j])
+                elif (int(seg[1]) < 2100) & (seg[1][0] != '0'):
+                    output[j] = str(seg[0]) + ' ' + dateyear(seg[1])
+                else: 
+                    n = ' '.join(seg[1])
+                    n = re.sub('0','o',n)
+                    n = re.sub('1','one',n)
+                    n = re.sub('2','two',n)
+                    n = re.sub('3','three',n)
+                    n = re.sub('4','four',n)
+                    n = re.sub('5','five',n)
+                    n = re.sub('6','six',n)
+                    n = re.sub('7','seven',n)
+                    n = re.sub('8','eight',n)
+                    n = re.sub('9','nine',n)
+                    output[j] = str(seg[0]) + ' ' + n  
+
+            else:     
+                output[j] = seg[0]
+            if ((seg[0] == 'u s')| (seg[0] == 's r') | (seg[0] == 'c r') | (seg[0] == 'i')) & (len(str(seg[1])) > 4):
+                output[j] = str(seg[0]) + ' ' + num2words(int(str(seg[1])))
+                output[j] = re.sub('-',' ',output[j]) 
+                output[j] = re.sub(',','',output[j]) 
+            elif ((seg[0] == 'u s')| (seg[0] == 's r') | (seg[0] == 'c r') | (seg[0] == 'i')) & (len(str(seg[1])) <= 4) & (len(seg) <= 2):
+                output[j] = str(seg[0]) + ' ' + dateyear(int(str(seg[1])))
+                output[j] = re.sub('-',' ',output[j]) 
+                output[j] = re.sub(',','',output[j])
+            output[j] = re.sub(' and','',output[j]) 
+        except ValueError:
+            output[j]='AAAis is wrong!'
+        except TypeError:
+            output[j]='AAAis is wrong!'
+        except IndexError:
+            output[j]='AAAis is wrong!'
     return output
